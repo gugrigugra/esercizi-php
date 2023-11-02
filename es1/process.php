e2a3da6113afdc1485f52eef24f4dcba2e4a7913
Gabriele Di Cosmo <115352720+gabrieleCoding1@users.noreply.github.com>
2023-11-02 08:35:38 +0100

    es1

---
+++ es1/process.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$nome = $_POST["nome"];
+$cognome = $_POST["cognome"];
+$altezza = $_POST["altezza"];
+$peso = $_POST["peso"];
+
+echo"nome: ".$nome."<br>cognome: ".$cognome."<br>altezza: ".$altezza."<br>peso: ".$peso."kg";
+
+?>
